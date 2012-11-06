0c7091af9ce3672058b5a35447231a88d00b82aa
Olivier Debeir <odebeir@ulb.ac.be>
2012-11-06 11:00:09 +0100

    remove problematic dependency

--- doc/conf.py
+++ doc/conf.py
@@ -31,7 +31,6 @@ extensions = [
     'sphinx.ext.autodoc',
     'ipython_directive',
     'matplotlib.sphinxext.ipython_console_highlighting',
-    'sphinxcontrib.programoutput'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
